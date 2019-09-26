8036af691cc90aa20c75e53c673fd3ab4f0a092d
Charlie Oster <oster.charlie@yahoo.com>
2019-09-26 21:13:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,8 +30,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  adding shipping_manifest["muskets"] = 2 
-  adding shipping_manifest["gun powder"] = 4
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4
   
 
   puts shipping_manifest
