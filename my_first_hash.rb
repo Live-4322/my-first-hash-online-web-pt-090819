135b29879051eefcec37b2bc5bd07977751795aa
Charlie Oster <oster.charlie@yahoo.com>
2019-09-26 21:55:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,10 +30,10 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest.store["muskets"] = 2 
-  shipping_manifest.store["gun powder"] = 4
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4
   
 
-  puts shipping_manifest
+  shipping_manifest
 
 end
